f60091968c6db55e1392242fe7a2d43a45ca7057
viral.islania <viral.islania@capgemini.com>
2013-02-20 22:55:56 +0530

    loyaltypoints entity

--- src/com/golo/app/model/LoyaltyPoints.java
+++ src/com/golo/app/model/LoyaltyPoints.java
@@ -2,12 +2,21 @@ package com.golo.app.model;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class LoyaltyPoints implements Model
 {
-   public class MerchantLoyaltyPoints
+   private static final String USERID = "userId";
+   private static final String TOTAL_LOYALTY_POINTS = "totalLoyaltyPoints";
+   private static final String MERCHANT_LOYALTY_POINTS = "merchantLoyaltyPointsList";
+
+   public class MerchantLoyaltyPoints implements Model
    {
+      private static final String MERCHANT_ID = "merchantId";
+      private static final String MERCHANT_NAME = "merchantName";
+      private static final String MERCHANT_LOYALTY_POINTS = "merchantLoyaltyPointsList";
+
       private int merchantId;
       private String merchantName;
       private int merchantLoyaltyPoints;
@@ -42,12 +51,29 @@ public class LoyaltyPoints implements Model
          this.merchantLoyaltyPoints = merchantLoyaltyPoints;
       }
 
+      @Override
+      public JSONObject serializeJSON() throws Exception
+      {
+         return null;
+      }
+
+      @Override
+      public void deserializeJSON(JSONObject jsonObject) throws Exception
+      {
+         if(jsonObject.has(MERCHANT_ID))
+            setMerchantId(jsonObject.getInt(MERCHANT_ID));
+         if(jsonObject.has(MERCHANT_NAME))
+            setMerchantName(jsonObject.getString(MERCHANT_NAME));
+         if(jsonObject.has(MERCHANT_LOYALTY_POINTS))
+            setMerchantLoyaltyPoints(jsonObject.getInt(MERCHANT_LOYALTY_POINTS));
+      }
+
    }
 
    private String userId;
    private int totalLoyaltyPoints;
-   private ArrayList<MerchantLoyaltyPoints> merchantLoyaltyPoints;
-   
+   private ArrayList<MerchantLoyaltyPoints> merchantLoyaltyPointsList;
+
    public int getTotalLoyaltyPoints()
    {
       return totalLoyaltyPoints;
@@ -57,7 +83,7 @@ public class LoyaltyPoints implements Model
    {
       this.totalLoyaltyPoints = totalLoyaltyPoints;
    }
-   
+
    public String getUserId()
    {
       return userId;
@@ -68,14 +94,14 @@ public class LoyaltyPoints implements Model
       this.userId = userId;
    }
 
-   public ArrayList<MerchantLoyaltyPoints> getMerchantLoyaltyPoints()
+   public ArrayList<MerchantLoyaltyPoints> getMerchantLoyaltyPointsList()
    {
-      return merchantLoyaltyPoints;
+      return merchantLoyaltyPointsList;
    }
 
-   public void setMerchantLoyaltyPoints(ArrayList<MerchantLoyaltyPoints> merchantLoyaltyPoints)
+   public void setMerchantLoyaltyPointsList(ArrayList<MerchantLoyaltyPoints> merchantLoyaltyPointsList)
    {
-      this.merchantLoyaltyPoints = merchantLoyaltyPoints;
+      this.merchantLoyaltyPointsList = merchantLoyaltyPointsList;
    }
 
    @Override
@@ -87,6 +113,21 @@ public class LoyaltyPoints implements Model
    @Override
    public void deserializeJSON(JSONObject jsonObject) throws Exception
    {
+      if (jsonObject.has(USERID))
+         setUserId(jsonObject.getString(USERID));
+      if (jsonObject.has(TOTAL_LOYALTY_POINTS))
+         setTotalLoyaltyPoints(jsonObject.getInt(TOTAL_LOYALTY_POINTS));
+      if (jsonObject.has(MERCHANT_LOYALTY_POINTS))
+      {
+         JSONArray jsonArray = jsonObject.getJSONArray(MERCHANT_LOYALTY_POINTS);
+         for (int i = 0; i < jsonArray.length(); i++)
+         {
+            JSONObject json = jsonArray.getJSONObject(i);
+            MerchantLoyaltyPoints merchantLoyaltyPoints = new MerchantLoyaltyPoints();
+            merchantLoyaltyPoints.deserializeJSON(json);
+            merchantLoyaltyPointsList.add(merchantLoyaltyPoints);
+         }
+      }
    }
 
 }
